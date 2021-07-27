181e8fbd95cc67bba85c2d6d510bfd297039556e
Reinaldy Rafli <aldy505@tutanota.com>
2021-07-27 23:30:02 +0700

    feat: help command

--- src/app.js
+++ src/app.js
@@ -5,18 +5,38 @@ import dotenv from 'dotenv';
 import redis from 'redis';
 import poll from './services/poll.js';
 import logger from './utlis/logger.js';
+import help from './services/help.js';
 
 const envPath = resolve(dirname(fileURLToPath(import.meta.url)), '../.env');
 dotenv.config({ path: envPath });
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
-
 const cache = redis.createClient(String(process.env.REDIS_URL));
 
 bot.on('message', async (context) => {
   try {
+    // For poll related
     if (context.message?.poll) {
       await poll(context, cache);
+      return;
+    }
+
+    // For message related (including /commands@teknologiumumbot)
+    if (context.message?.text) {
+      if (context.message.text.startsWith('/') && context.message.text.includes(context.me)) {
+        const args = context.message.text.slice(1).split(/ +/);
+        const commands = args.shift().replace(`@${context.me}`, '').toLowerCase();
+
+        switch (commands) {
+          case 'help':
+            await help(context);
+            break;
+          default:
+            context.reply(`Sorry, I can't recognize the command.`);
+        }
+        return;
+      }
+      return;
     }
   } catch (error) {
     await context.reply('uh oh, something went wrong. ask the devs to check their logs');
---
+++ src/services/help.js
@@ -0,0 +1,22 @@
+/**
+ * Send help to user when needed.
+ * @param {import('telegraf').Context} context
+ * @returns {Promise<void>}
+ */
+async function help(context) {
+  try {
+    await context.reply(
+      `Hi there! I can't do much right now. I only could pin and manage poll on this channel.\n\n` +
+        `For feature request please refer to the <a href="https://github.com/teknologi-umum/bot">Github repository</a>.`,
+      {
+        parse_mode: 'HTML',
+        reply_to_message_id: context.message.chat.id,
+      },
+    );
+    return Promise.resolve()
+  } catch (error) {
+    Promise.reject(error);
+  }
+}
+
+export default help;
