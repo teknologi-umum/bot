8b1b18ea5b66552ff235fdc1917dd4ec3f18bf5f
Reinaldy Rafli <aldy505@tutanota.com>
2021-08-02 15:54:17 +0700

    feat: memes

---
+++ src/services/meme.js
@@ -0,0 +1,26 @@
+/**
+ * Send meme.
+ * @param {String} input
+ * @param {import('telegraf').Context} context
+ * @returns {Promise<void>}
+ */
+async function meme(input, context) {
+  try {
+    switch (input) {
+      case 'kktbsys':
+        await context.telegram.sendPhoto(context.message.chat.id, 'https://i.ibb.co/XtSbXBT/image.png');
+        break;
+      case 'illuminati':
+        // Sources: https://giphy.com/search/illuminati
+        await context.telegram.sendAnimation(context.message.chat.id, 'https://media.giphy.com/media/uFOW5cbNaoTaU/giphy.gif');
+        break;
+      default:
+        break;
+    }
+    return Promise.resolve()
+  } catch (error) {
+    return Promise.reject(error)
+  }
+}
+
+export default meme
