bfd27ad5b8a3efd374e37bc0d094650c0c70d0ea
Elsa Purwani <47468136+ywnn@users.noreply.github.com>
2021-08-06 02:50:49 +0700

    fix(request body): failed generate random seed because failed parse json

--- src/services/meme.js
+++ src/services/meme.js
@@ -4,8 +4,8 @@
  * @returns {Promise<void>}
  */
 
- import { request } from 'undici';
- import { randomNumber } from 'carret';
+import { request } from 'undici';
+import { randomNumber } from 'carret';
 
 export function register(bot) {
   bot.command('kktbsys', async (context) => {
@@ -22,12 +22,12 @@ export function register(bot) {
   bot.command('yntkts', async (context) => {
     await context.telegram.sendPhoto(context.message.chat.id, 'https://i.ibb.co/P1Q0650/yntkts.jpg');
   });
-  
-   bot.command('joke', async (context) => {
-    let apis = "https://jokesbapak2.herokuapp.com/v1";
-    const {body} = await request(apis + "/total")
+
+  bot.command('joke', async (context) => {
+    let apis = 'https://jokesbapak2.herokuapp.com/v1';
+    const { body } = await request(apis + '/total');
     let total = parseInt((await body.json()).message);
     let uid = randomNumber(0, total).toString();
-    await context.telegram.sendPhoto(context.message.chat.id, ("https://jokesbapak2.herokuapp.com/v1/id/"+ uid));
+    await context.telegram.sendPhoto(context.message.chat.id, 'https://jokesbapak2.herokuapp.com/v1/id/' + uid);
   });
 }
