b30d2eda8fdf6c54ff04770ccd60c18776efa9f3
Reinaldy Rafli <aldy505@tutanota.com>
2021-08-07 17:51:10 +0700

    fix(eval): added command with username mention

--- src/services/eval/index.js
+++ src/services/eval/index.js
@@ -12,6 +12,8 @@ async function evalCommand(context) {
   let source;
   if (text.startsWith('/eval ')) {
     source = text.substring(6);
+  } else if (text.startsWith(`/eval@${context.me} `)) {
+    source = text.substring(7 + context.me.length);
   } else if (text.startsWith('```') && text.endsWith('```')) {
     source = text.substring(3, text.length - 4);
   } else {
