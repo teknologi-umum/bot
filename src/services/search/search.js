464db1e26b951d1780bf133101b28de5aedf3b1c
Reinaldy Rafli <aldy505@tutanota.com>
2021-09-02 20:32:18 +0700

    hotfix: search cheerio error

--- src/services/search/search.js
+++ src/services/search/search.js
@@ -33,9 +33,9 @@ async function search(context, mongo) {
 
   let items = $('.web-result')
     .map((_, el) => {
-      const title = el('.result__title > a').first().text() || 'Failed to get title';
-      const href = decodeURIComponent(cleanURL(el('.result__title > a').first().attr('href')));
-      const snippet = el('.result__snippet')
+      const title = $(el).find('.result__title > a').first().text() || 'Failed to get title';
+      const href = decodeURIComponent(cleanURL($(el).find('.result__title > a').first().attr('href')));
+      const snippet = $(el).find('.result__snippet')
         .map((_, el) => el.children.map((x) => $.html(x)).join(''))
         .get();
 
