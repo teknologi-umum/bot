850084141bec28d17d485711856648384c04848b
Reinaldy Rafli <aldy505@proton.me>
2023-05-15 07:33:34 +0700

    fix: should respect new lines (#190)

--- tests/command.test.js
+++ tests/command.test.js
@@ -46,4 +46,26 @@ test("should return empty string if no params was given", () => {
   assert.equal(argument, "");
 });
 
+test("should handle new lines", () => {
+  const fakeContext = {
+    message: { text: "/foo\nbar\nbaz" },
+    me: "teknologiumumbot"
+  };
+
+  const argument = getCommandArgs("foo", fakeContext);
+
+  assert.equal(argument, "bar\nbaz");
+});
+
+test("should handle complicated new lines", () => {
+  const fakeContext = {
+    message: { text: "/js\r\nasync function hello() {\n\tconst logger = new Logger();\r\n\tlogger.log(\"Hello world\");\r};\n" },
+    me: "teknologiumumbot"
+  };
+
+  const argument = getCommandArgs("js", fakeContext);
+
+  assert.equal(argument, "async function hello() {\n\tconst logger = new Logger();\r\n\tlogger.log(\"Hello world\");\r};");
+});
+
 test.run();
